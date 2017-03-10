06b61d37b2b32322d3ff1eb9ddc10f660f59dd90
Wolfram Eberius <eberius@gmail.com>
2017-03-10 06:05:43 +0100

    Oberfläche aufräumen #8

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -221,6 +221,7 @@ function onLocationFound(e) {
     var stationurl = "https://flask.cologne.codefor.de/database?lat=" + e.latlng.lat +"&long=" + e.latlng.lng + "";
     $.getJSON(stationurl, function (data) {
       museums.addData(data);
+      map.addLayer(museumLayer);
     });
 
 }
@@ -276,38 +277,6 @@ map.addControl(attributionControl);
 
 var zoomControl = L.control.zoom({
   position: "bottomright"
-}).addTo(map);
-
-/* GPS enabled geolocation control set to follow the user's location */
-var locateControl = L.control.locate({
-  position: "bottomright",
-  drawCircle: true,
-  follow: true,
-  setView: true,
-  keepCurrentZoomLevel: false,
-  markerStyle: {
-    weight: 1,
-    opacity: 0.8,
-    fillOpacity: 0.8
-  },
-  circleStyle: {
-    weight: 1,
-    clickable: false
-  },
-  icon: "fa fa-location-arrow",
-  metric: false,
-  strings: {
-    title: "My location",
-    popup: "You are within {distance} {unit} from this point",
-    outsideMapBoundsMsg: "You seem located outside the boundaries of the map"
-  },
-  locateOptions: {
-    maxZoom: 16,
-    watch: true,
-    enableHighAccuracy: true,
-    maximumAge: 10000,
-    timeout: 10000
-  }
 }).addTo(map);
 
 /* Larger screens get expanded layer control and visible sidebar */
